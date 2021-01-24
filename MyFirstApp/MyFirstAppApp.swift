ae3324b385db8c468760dd2f6276e6da1927f396
albertjordan <albertjordan@mac.com>
2021-01-23 21:14:52 -0800

    Initial Commit

---
+++ MyFirstApp/MyFirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstAppApp.swift
+//  MyFirstApp
+//
+//  Created by Albert Jordan on 1/23/21.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
