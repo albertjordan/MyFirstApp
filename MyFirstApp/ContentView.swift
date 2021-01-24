6c6b841be8d97d348e77b96ee7c0f8525402b4f2
albertjordan <albertjordan@mac.com>
2021-01-23 22:54:04 -0800

    playing...

--- MyFirstApp/ContentView.swift
+++ MyFirstApp/ContentView.swift
@@ -9,8 +9,19 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, Albert!")
-            .padding()
+        VStack{
+            Text("Hello, world!")
+                .padding()
+            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                /*@START_MENU_TOKEN@*/Text("Button")/*@END_MENU_TOKEN@*/
+            })
+        }
+        
     }
 }
 
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
