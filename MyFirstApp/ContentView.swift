ddb299e852acf906795d0fa53742490274497ef5
albertjordan <albertjordan@mac.com>
2021-01-23 22:07:44 -0800

    Just changed Hello world to Albert

--- MyFirstApp/ContentView.swift
+++ MyFirstApp/ContentView.swift
@@ -9,13 +9,8 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        Text("Hello, Albert!")
             .padding()
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
